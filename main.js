d4344852478de6d754501ad171d9c08deb2484c2
wookki <bwlimtony@gmail.com>
2022-05-16 10:50:53 +0900

    find string

--- main.js
+++ main.js
@@ -1,20 +1,8 @@
-function solution(arr) {
-  let answer = [];
-  let sum = 0, min = Number.MAX_SAFE_INTEGER
+function solution(s, t){
+  let answer = s.split(t).length
   
-  for(let x of arr) {
-    if(x % 2 === 1) {
-      sum += x;
-      if(x < min) {
-        min = x;
-      }
-    }
-  }
-  
-  answer.push(sum);
-  answer.push(min);
-  return answer;
+  return answer - 1;
 }
 
-let arr = [12, 77, 38, 41, 53, 92, 85]
-console.log(solution(arr));
+let str = "COMPUTERPROGRAMMING";
+console.log(solution(str, 'R'));
